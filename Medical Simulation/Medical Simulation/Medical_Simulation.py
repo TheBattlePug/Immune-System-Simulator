bbc4039d85e0002565785c86a618a1dbf6248928
Dan Dumitrescu <dan.dumitrescu@LIVE.COM>
2019-03-06 19:59:21 -0800

    create new infection

--- Medical Simulation/Medical Simulation/Medical_Simulation.py
+++ Medical Simulation/Medical Simulation/Medical_Simulation.py
@@ -2,7 +2,10 @@ from graphics import *
 from random import *
 import time
 
-win = GraphWin("Human Body", 200, 200)
+windowHeight = 400
+windowWidth = 400
+
+win = GraphWin("Human Body", windowHeight, windowWidth)
 
 class Cell:
     def __init__ (self, state, x, y):
@@ -23,70 +26,58 @@ x1 = 50
 y1 = 100
 pathogen1 = Pathogen(x1, y1)
 
-#def infectCells():
-    #global drawhealthyCell
-   # global healthyCell
-
-   # if (x1 >= 55):
-    #          drawhealthyCell.undraw()
-     #         drawhealthyCell = Circle(Point(healthyCell.X, healthyCell.Y), 10)
-         #     drawhealthyCell.setOutline('red')
-          #    drawhealthyCell.draw(win)
-           #   healthyCell.State = "infected"
-    #print("The cells are now ")
-
-	#Test Comment
-
-
-    #Another comment
+# Creates a matrix of a defined height and width
+h = 6
+w = 6;
+Matrix = [[Cell("healthy", randrange(20, windowHeight-20),randrange(20, windowWidth-20)) for x in range(h)] for y in range(w)] 
 
 def drawCell():
     global x1
     global x2
-    for y in range(10,190,25):
-        for x in range(110,200,25):
-           healthyCell = Cell("healthy", x, y)
-           drawhealthyCell = Circle(Point(healthyCell.X, healthyCell.Y), 10)
-           drawhealthyCell.draw(win)
-           if (x1 >= 55):
-               healthyCell.State = "infected"
-           if (healthyCell.State == "infected"):
-               drawhealthyCell.undraw()
-               drawhealthyCell = Circle(Point(healthyCell.X, healthyCell.Y), 10)
-               drawhealthyCell.setOutline('red')
-               drawhealthyCell.draw(win)
+    for i in range(0,h):
+        for j in range(0,w):
+           cell = Matrix[i][j]
+           drawCell = Circle(Point(cell.X, cell.Y), 10)
+           drawCell.draw(win)
 
-               
+           if (abs(x1-cell.X)<=15 and abs(y1-cell.Y)<=15):
+               cell.State = "infected"
+
+           if (cell.State == "infected"):
+               drawCell.undraw()
+               drawCell = Circle(Point(cell.X, cell.Y), 10)
+               drawCell.setOutline('red')
+               drawCell.draw(win)
+              
 
 
-def drawPathogen():
+def draw():
     global x1
     global y1
     global pathogen1
-    global drawhealthyCell
-    
+        
     p = Point(pathogen1.X, pathogen1.Y)
     path = Circle(p, 5)
     path.draw(win)
     path.setFill('red')
 
     while True:
-         a = randrange(-2,10)
+         a = randrange(-10,20)
          x1 = x1 + a
-         b = randrange(-10,10)      
+         b = randrange(-10,20)      
          y1 = y1 + b
 
-         if (x1 > 200):
+         if (x1 > windowWidth):
              x1 = 0
              
          if (x1 < 0):
-             x1 = 200
+             x1 = windowWidth
              
-         if (y1 > 200):
+         if (y1 > windowHeight):
              y1 = 0
                 
          if (y1 < 0):
-             y1 = 200
+             y1 = windowHeight
          
          path.undraw()
          path = Circle(Point(x1,y1), 5)
@@ -94,12 +85,12 @@ def drawPathogen():
          path.draw(win)
 
          drawCell()
-         time.sleep(0.5)
+         time.sleep(0.2)
          path.move(a,b)
          printPoint(x1,y1)
          
 
-drawPathogen()
+draw()
 
 
 
