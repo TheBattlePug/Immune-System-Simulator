f775548f92e44dc585cf95af4830e4011755add0
DAN DUMITRESCU <s-DDUMITRESCU@lwsd.org>
2019-12-06 21:29:48 -0800

    working line

--- Climate_Change_Project/Climate_Change_Project/MainWindow.xaml.cs
+++ Climate_Change_Project/Climate_Change_Project/MainWindow.xaml.cs
@@ -20,9 +20,56 @@ namespace Climate_Change_Project
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        Line cover;
         public MainWindow()
         {
             InitializeComponent();
+          
+        }
+
+        private void SliderLatitude_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
+        }
+
+        private void SliderLongitude_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
+        }
+
+        private void SliderDistance_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            cover = new Line();
+
+            //cover.X1 = Earth.Margin.Left /* - DistanceSlider.Value;*/;
+            //cover.Y1 = Earth.Margin.Top + Earth.ActualHeight / 2 - 50 ;
+
+            //cover.X2 = Earth.Margin.Left /* - DistanceSlider.Value;*/;
+            //cover.Y2 = Earth.Margin.Top + Earth.ActualHeight / 2 + 50;
+
+            cover.X1 = 0;
+            cover.Y1 = 0;
+
+            cover.X2 = canvas.ActualWidth;
+            cover.Y2 = canvas.ActualHeight;
+
+           
+            cover.StrokeThickness = 1;
+            cover.Stroke = Brushes.Black;
+
+            canvas.Children.Add(cover);
+
+        }
+
+        private void SliderTemperature_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SliderDistance_ValueChanged(null, null);
         }
     }
 }
