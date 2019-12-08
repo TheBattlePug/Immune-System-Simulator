eb7f74d5f2ae8365d6dc2aae59edfa39c03ea63d
Dan Dumitrescu <dan.dumitrescu@LIVE.COM>
2019-12-07 20:44:57 -0800

    Added a way to move the screen

--- Climate_Change_Project/Climate_Change_Project/MainWindow.xaml.cs
+++ Climate_Change_Project/Climate_Change_Project/MainWindow.xaml.cs
@@ -36,19 +36,27 @@ namespace Climate_Change_Project
         private void SliderLongitude_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
 
+            Thickness m = Screen.Margin;
+
+            m.Top = 2 * e.NewValue + 144;
+
+            Screen.Margin = m;
+            
         }
 
         private void SliderDistance_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
+            Thickness m = Screen.Margin;
             
+            m.Left = 15 * e.NewValue + 250;
 
+            Screen.Margin = m;
         }
 
         private void SliderTemperature_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
+           
 
-        }
-
-       
+        }       
     }
 }
